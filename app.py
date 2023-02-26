7e15744561e51e7b6b5798df3b3b0b9bbd807c5b
Jose Fco de la Fuente <josefcodelafuente@gmail.com>
2023-02-26 18:19:22 +0100

    ml funciona ok

--- app.py
+++ app.py
@@ -2,22 +2,31 @@ import numpy as np
 from flask import Flask, request, render_template
 import pickle
 
+#Create an app object using the Flask class. 
 app = Flask(__name__)
 
+#Load the trained model. (Pickle file)
 model = pickle.load(open('models/model.pkl', 'rb'))
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/questions')
+def questions():
+    return render_template('questions.html')
+
 @app.route('/predict',methods=['POST'])
 def predict():
     int_features = [int(x) for x in request.form.values()]
-    features = [np.array(int_features)]  
-    prediction = model.predict(features) 
+    features = [np.array(int_features)]  #Convert to the form [[a, b]] for input to the model
+    print(features)
+    prediction = model.predict(features)  # f
     result = prediction[0]
+    print(result)
 
-    return render_template('index.html', prediction=result)
+
+    return render_template('results.html', result=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
